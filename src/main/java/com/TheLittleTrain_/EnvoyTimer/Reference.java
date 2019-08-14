355d3b0432d4e3b659b9a6310bba0130db38ad0b
TheLittleTrain-cosmic <54089839+TheLittleTrain-cosmic@users.noreply.github.com>
2019-08-14 01:52:52 -0400

    Update Reference.java

--- src/main/java/com/TheLittleTrain_/EnvoyTimer/Reference.java
+++ src/main/java/com/TheLittleTrain_/EnvoyTimer/Reference.java
@@ -3,7 +3,7 @@ package com.TheLittleTrain_.EnvoyTimer;
 public class Reference {
     public static final String MOD_ID = "EnvoyTimer";
     public static final String NAME = "Cosmic Envoy Timer";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String MINECRAFT_VERSION = "[1.8.9]";
 
 
